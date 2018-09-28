efcc733b9c85fee9b8c00080c2df1f8b708968c6
Siti Sofiah Mutia <43495797+sofiamutiast@users.noreply.github.com>
2018-09-28 10:09:10 +0700

    proses
    hanya bisa menampilkan checkbox

--- proses.php
+++ proses.php
@@ -1,4 +1,21 @@
+<form method="post">
+Gambar : <input type="file" name="gambar"> <br>
+<input type="checkbox" name="hobi[]" alt="Checkbox" value="Futsal"> Futsal <br>
+<input type="checkbox" name="hobi[]" alt="Checkbox" value="Memancing">  Memancing <br>
+<input type="checkbox" name="hobi[]" alt="Checkbox" value="Membaca"> Membaca <br>
+<input type="checkbox" name="hobi[]" alt="Checkbox" value="Berenang"> Berenang <br>
+<input type="checkbox" name="hobi[]" alt="Checkbox" value="Menulis"> Menulis <br>
+<input type="submit"   name="submit">
+</form>
+
 <?php
+if(isset($_POST['submit'])){
+	foreach($_POST['hobi'] as $data){
+      
+      echo $data ."<br/>";
 
-
+	}
+}
+	
+ 
 ?>
